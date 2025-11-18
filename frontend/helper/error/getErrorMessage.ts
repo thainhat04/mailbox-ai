920c092d11f5a8aa5c5e0385fb2e669baa7aada8
TNMonster <nhatmonsterhack@gmail.com>
2025-11-19 02:58:40 +0700

    fix deploy

--- frontend/helper/error/getErrorMessage.ts
+++ frontend/helper/error/getErrorMessage.ts
@@ -1,9 +1,7 @@
-import { isAuthErrorCustom, isCustomError } from "@/helper/error/deprecated";
+import { isCustomError } from "@/helper/error/deprecated";
 import constant from "@/constants/config";
 export default function getErrorMessage(error: unknown): string {
-    if (isAuthErrorCustom(error)) {
-        return error.data.error;
-    } else if (isCustomError(error)) {
+    if (isCustomError(error)) {
         return error.data as string;
     } else {
         return constant.ERROR_DEFAULT;
